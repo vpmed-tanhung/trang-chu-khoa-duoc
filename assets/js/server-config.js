247addb385a724138b5d2d50f74291a431ff8f18
vpmed-tanhung <hoangphamcv1997@gmail.com>
2026-09-05 13:36:27 +0700

    cập nhật
    assets/js/server-config.js

--- assets/js/server-config.js
+++ assets/js/server-config.js
@@ -2,8 +2,8 @@
   'use strict';
 
   window.KHOA_DUOC_SERVER = Object.freeze({
-    supabaseUrl: '',
-    supabasePublishableKey: '',
+    supabaseUrl: 'https://vrpfwmyinhippwrzmcid.supabase.co',
+    supabasePublishableKey: 'sb_publishable_-VY5QRli7TcYRESFcO_now_-2cOdEn7',
     storageBucket: 'drug-documents',
     maxUploadBytes: 26214400
   });
