fa5408c442fdd55f94ab596fefe5bbf99921f9aa
vpmed-tanhung <hoangphamcv1997@gmail.com>
2026-09-10 22:34:56 +0700

    fix lỗi AI

--- assets/js/server-config.js
+++ assets/js/server-config.js
@@ -5,7 +5,7 @@
     appVersion: '1.1.8',
     supabaseUrl: 'https://vrpfwmyinhippwrzmcid.supabase.co',
     supabasePublishableKey: 'sb_publishable_-VY5QRli7TcYRESFcO_now_-2cOdEn7',
-    clinicalReviewWebAppUrl: 'https://script.google.com/macros/s/AKfycbyeLZslT5IKRwePrRY3m-k2zlcFLJwsSjDh6etvbihNwQY9UqjgM3BPgN5hRJX9GAX7hg/exec',
+    clinicalReviewWebAppUrl: 'https://script.google.com/macros/s/AKfycby0yk02lBKLWzXadlTg60Qoy6bGLrKW5A6AeGtCuoWS7IsImQknz2BI3GbZeZzSN665/exec',
     storageBucket: 'drug-documents',
     maxUploadBytes: 26214400
   });
